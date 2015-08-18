5f09e2b54042b7fc163e00eb2c805d2cd0cad990
germanogualberto <gmanopoliano@gmail.com>
2015-08-18 03:08:22 -0300

    adicionado arquivos que faltavam para a tela de cadastro funcionar

--- app/src/main/java/br/edu/ufcg/embedded/syndiagnosis/CadastroMedicoActivity.java
+++ app/src/main/java/br/edu/ufcg/embedded/syndiagnosis/CadastroMedicoActivity.java
@@ -16,7 +16,7 @@ public class CadastroMedicoActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_cadastro_medico, menu);
+        getMenuInflater().inflate(R.menu.menu_cadastro_medico, menu);
         return true;
     }
 
